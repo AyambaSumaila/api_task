c48a76d2576270aedddfc2e316cdfe5094b23c56
Ayamba Sumaila <sumailaayamba15@gmail.com>
2024-12-27 00:41:53 +0000

    Update serializers.py

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -2,13 +2,17 @@ from rest_framework import serializers  # type: ignore
 
 from .models import Products
 
-class ProductsSerializer(serializers.ModelSerializer):
+class ProductsListSerializer(serializers.ModelSerializer):
    class Meta:
        model = Products
        fields = ('id', 'name', 'price', 'quantity', 'description', 'created_by')
        
        
        
+class ProductsDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = ('id', 'name', 'price', 'quantity', 'description', 'created_by')         
        
        
        
