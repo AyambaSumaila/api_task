3a80cad3534b344299247c1394fbea0e2b4864ca
Ayamba Sumaila <sumailaayamba15@gmail.com>
2024-12-27 00:42:25 +0000

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path 
-from .views import ProductsList, ProductsDetail 
+#from .views import ProductsListAPIView, ProductsDetailAPIView 
 
-
+from .import views 
 
 urlpatterns = [
-     path('', ProductsList.as_view(), name='products-list'),
-     path('<int:pk>', ProductsDetail.as_view(), name='products-detail'),
+     path('', views.ProductsListAPIView.as_view(), name='product_list'),
+     path('<int:id>/', views.ProductsRetrieveAPIView.as_view(), name='product_detail'),
+     path('create/', views.ProductsCreateAPIView.as_view(), name='product_create'),
+     path('update/<int:id>/', views.ProductsRetrieveUpdateAPIView.as_view(), name="product_update"),
+     path('delete/<int:id>/', views.ProductsDestroyAPIView.as_view(), name="product_delete"),
 
 ]
 
